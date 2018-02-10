3ebe96a5d82330629c4d247c50abe9fbeca4b523
Michelle Brien <michelle.brien12@gmail.com>
2018-02-10 14:34:03 +0000

    responsive card added

---
+++ app/javascript/components/EndorsementCard/EndorsementCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container, Card, Segment, Group } from 'semantic-ui-react';
+
+
+export default class EndorsementCard extends React.Component {
+    render() {
+        return (
+            <div>
+                <Container text className ="card-center">
+                    <Segment >
+                        <h1>Fuck this</h1>
+                    </Segment>
+                </Container>
+            </div>
+        );
+    }
+}
