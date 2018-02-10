78e10bf6dd9fe8bf002b306fdb820f88ed748d9c
Michelle Brien <michelle.brien12@gmail.com>
2018-02-10 17:09:19 +0000

    added button

--- app/javascript/components/EndorsementCard.jsx
+++ app/javascript/components/EndorsementCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Card, Segment, Header } from 'semantic-ui-react';
+import { Container, Card, Segment, Header, Button } from 'semantic-ui-react';
 import RangeSlider from './RangeSlider';
 
 
@@ -9,10 +9,15 @@ export default class EndorsementCard extends React.Component {
         return (
             <div>
                 <Container text className ="card-center">
-                    <Segment className="flex-container-center">
-                        <Header as='h1'>Endorsements</Header>
-                        <p>How <strong>reliable</strong> is Harriet?</p>
-                            <RangeSlider/>    
+                    <Segment>
+                        <div className="flex-container-center">
+                            <Header as='h1'>Endorsements</Header>
+                            <p>How <strong>reliable</strong> is Harriet?</p>
+                            <RangeSlider/> 
+                        </div>
+                        <div className="flex-container-right">
+                            <Button basic>Next</Button> 
+                        </div>
                     </Segment>
                 </Container>
             </div>
