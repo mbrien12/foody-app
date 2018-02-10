e38ad61711965e568e345c7318c5a8c7fd9a6513
Michelle Brien <michelle.brien12@gmail.com>
2018-02-10 18:01:28 +0000

    working on updating behaviour content

--- app/javascript/components/EndorsementCard.jsx
+++ app/javascript/components/EndorsementCard.jsx
@@ -17,28 +17,31 @@ export default class EndorsementCard extends React.Component {
              Listening: "good at listening",
              Reliable: "reliable"
             };
+    
         this.handleSubmit = this.handleSubmit.bind(this);
       }
 
       handleSubmit = event => {
-          console.log('submitted');
+        console.log(behaviour[1]);
       }
 
     render() {
+        const behaviour = ['adaptable', 'collaborative', 'communicative', 'diplomatic', 'empathetic', 'inclusive', 'good at listening', 'reliable'  ]
+    
         return (
             <div>
                 <Container text className ="card-center">
                     <Segment>
                         <div className="flex-container-center">
                             <Header as='h1'>Endorsements</Header>
-                            <p className="endorsement-question">On a scale of 1-10, how <strong>{this.state.Listening}</strong> is Harriet?</p>
+                            <p className="endorsement-question">On a scale of 1-10, how <strong>{behaviour[0]}</strong> is Harriet?</p>
                             <RangeSlider/> 
                         </div>
-                        <div className="flex-container-right">
-                        <Form onSubmit={this.handleSubmit}>
-                            <Button basic type="Submit">Next</Button> 
-                        </Form>
-                        </div>
+                            <div className="flex-container-right">
+                                <Form onSubmit={this.handleSubmit}>
+                                    <Button basic type="Submit">Next</Button> 
+                                </Form>
+                            </div>
                     </Segment>
                 </Container>
             </div>
