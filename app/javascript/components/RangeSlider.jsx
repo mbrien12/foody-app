f90d623ba3e79f128aab3efa3509c9e45e88725b
Michelle Brien <michelle.brien12@gmail.com>
2018-02-17 22:01:25 +0000

    added middle add behaviour comp

--- app/javascript/components/RangeSlider.jsx
+++ app/javascript/components/RangeSlider.jsx
@@ -4,42 +4,48 @@ import ReactDOM from 'react-dom';
 import Slider from 'react-rangeslider' 
 // https://github.com/whoisandy/react-rangeslider
 
+import AddBehaviour from './AddBehaviour'
+
 export default class RangeSlider extends React.Component {
-    constructor (props, context) {
-        super(props, context)
-        this.state = {
-          value: 5
-        }
-      }
- 
-    handleChange = value => {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: 5,
+      behaviour: 'adaptable'
+    };
+  }
+
+  handleChange = value => {
     this.setState({
-        value: value,
-        behaviour: this.props.behaviour
-     })
+      value: value,
+      behaviour: this.props.behaviour
+    });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    const horizontalLabels = {
+      0: "Not at all",
+      10: "Extremely"
     };
 
-    render() {
-        const { value } = this.state;
-
-        const horizontalLabels = {
-            0: 'Not at all',
-            10: 'Extremely'
-          }
-
-        return (
-          <div className='slider'>
-            <Slider
-              min={0}
-              max={10}
-              value={value}
-              labels={horizontalLabels}
-              onChangeStart={this.handleChangeStart}
-              onChange={this.handleChange}
-              onChangeComplete={this.handleChangeComplete}
-            />
-            <div className='value'>{value}</div>
-          </div>
-        )
-      }
+    return (
+      <div>
+        <div className="slider">
+          <Slider
+            min={0}
+            max={10}
+            value={value}
+            labels={horizontalLabels}
+            onChangeStart={this.handleChangeStart}
+            onChange={this.handleChange}
+            onChangeComplete={this.handleChangeComplete}
+          />
+          <div className="value">{value}</div>
+          <AddBehaviour value={this.state.value} behaviour={this.state.behaviour}  />
+        </div>
+      </div>
+    );
+  }
 }
