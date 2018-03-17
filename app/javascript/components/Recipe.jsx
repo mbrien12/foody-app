702f6e4c91c90613b3666309c55517ecd5572533
Michelle Brien <michelle.brien12@gmail.com>
2018-03-17 18:49:29 +0000

    tab menu added

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Card, Container, Icon, Image } from "semantic-ui-react";
+import { Card, Container, Icon, Image, Tab } from "semantic-ui-react";
 
 export default class Recipe extends React.Component {
   render() {
     const recipe = this.props.recipe;
+    const panes = [
+      { menuItem: 'Ingredients', render: () => <Tab.Pane attached={false}>{recipe.ingredients}</Tab.Pane> },
+      { menuItem: 'Instructions', render: () => <Tab.Pane attached={false}>{recipe.instructions}</Tab.Pane> },
+    ]
     const cardLayout = {
       marginTop: "100px"
     };
@@ -24,7 +28,7 @@ export default class Recipe extends React.Component {
             </Card.Content>
             <Card.Content extra>
               <Card.Description>
-                {recipe.instructions}
+                <Tab menu={{ secondary: true }} panes={panes} />
               </Card.Description>
             </Card.Content>
           </Card>
