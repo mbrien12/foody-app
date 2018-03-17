885545bcb4139a946cbb15612107480a2b9cf2ab
Michelle Brien <michelle.brien12@gmail.com>
2018-03-17 16:34:39 +0000

    styled nav

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { Menu } from 'semantic-ui-react';
+import { Icon, Menu, Segment } from "semantic-ui-react";
 
 export default class Header extends React.Component {
-    render() {
-        return (
-        <Menu>
-            <Menu.Item>Home</Menu.Item>
+  render() {
+    return (
+      <Segment inverted>
+        <Menu stackable inverted secondary>
+          <Menu.Item position="right" href="/">
+            <Icon name="book" />
+            Recipes
+          </Menu.Item>
+          <Menu.Item>
+            <Icon name="food" />
+            Restaurants
+          </Menu.Item>
         </Menu>
-        );
-    }
+      </Segment>
+    );
+  }
 }
