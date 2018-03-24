a054d9a74f5bfb4e651e20f6c34d5a03e613808e
Michelle Brien <michelle.brien12@gmail.com>
2018-03-24 13:42:45 +0000

    added names and values

--- app/javascript/components/RecipeForm.jsx
+++ app/javascript/components/RecipeForm.jsx
@@ -17,23 +17,24 @@ export default class RecipeForm extends React.Component {
     //   fontWeight: 'lighter',
     //   fontSize: '18px'
     // }
-    
+    const { name, time, serving, url, ingredients, instructions, category, image} = this.state
+
     return (
       <Segment inverted>
       <Header as='h2'>Add recipe</Header>
       <Form inverted>
         <Form.Group widths='equal'>
-          <Form.Input fluid label='Recipe name' placeholder='Lasagna' />
-          <Form.Input fluid label='How does it take?' placeholder='40 mins' />
-          <Form.Input type='number' label='How many people is it for?' placeholder='4' />
+          <Form.Input fluid label='Recipe name' name='name' value={name} placeholder='Lasagna' />
+          <Form.Input fluid label='How does it take?' name='time' value={time} placeholder='40 mins' />
+          <Form.Input type='number' name='serving' value={serving} label='How many people is it for?' placeholder='4' />
         </Form.Group>
         <Form.Group widths='equal'>
-        <Form.Select fluid label='What type of meal is it?' options={options} placeholder='Lunch' />
-        <Form.Input fluid label='Where is it from?' placeholder='Guardian' />
+        <Form.Select fluid label='What type of meal is it?' options={options} name='category' value={category} placeholder='Lunch' />
+        <Form.Input fluid label='Where is it from?' name='url' value={url} placeholder='Guardian' />
         </Form.Group>
-        <Form.TextArea label='Ingredients' />
-        <Form.TextArea label='Instructions' />
-        <Form.Input fluid label='Recipe image url'/>
+        <Form.TextArea label='Ingredients' name='ingredients' value={ingredients} />
+        <Form.TextArea label='Instructions' name='instructions' value={instructions} />
+        <Form.Input fluid label='Recipe image url' name='image' value={image}/>
         <Button type='submit'>Submit</Button>
       </Form>
     </Segment>
