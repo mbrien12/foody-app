7f95cf68aefc77e5b81bc8d554ae82691c028469
Michelle Brien <michelle.brien12@gmail.com>
2018-03-24 16:13:21 +0000

    added props

--- app/javascript/components/RecipeForm.jsx
+++ app/javascript/components/RecipeForm.jsx
@@ -1,59 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Button, Form, Header,Segment } from "semantic-ui-react";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
+//https://www.npmjs.com/package/react-rails-form-helpers
 
 export default class RecipeForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: ''};
+    this.state = {
+      recipes: this.props.recipe
+    };
 
-    this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-    console.log(this.state.name);
-  }
-
-  render() {
-    const options = [
-      {key: 'b', text: 'Breakfast', value: 'breakfast'},
-      {key: 'l', text: 'Lunch', value: 'lunch'},
-      {key: 'd', text: 'Dinner', value: 'dinner'},
-      {key: 'de', text: 'Dessert', value: 'dessert'}
-    ]
-
-    //TODO: Fix label fonts
-    // const labelFont = {
-    //   fontWeight: 'lighter',
-    //   fontSize: '18px'
-    // }
-    this.state = { name: '', time: 0}
-    // const { name, time, serving, url, ingredients, instructions, category, image} = this.state
-
-    return (
-      <Segment inverted>
-      <Header as='h2'>Add recipe</Header>
-      <Form inverted>
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='Recipe name' name='name' value={name} onChange={this.handleChange}  placeholder='Lasagna' />
-          <Form.Input fluid label='How does it take?' name='time' value={time} placeholder='40 mins' />
-          <Form.Input type='number' name='serving' value={serving} label='How many people is it for?' placeholder='4' />
-        </Form.Group>
-        <Form.Group widths='equal'>
-        <Form.Select fluid label='What type of meal is it?' options={options} name='category' value={category} placeholder='Lunch' />
-        <Form.Input fluid label='Where is it from?' name='url' value={url} placeholder='Guardian' />
-        </Form.Group>
-        <Form.TextArea label='Ingredients' name='ingredients' value={ingredients} />
-        <Form.TextArea label='Instructions' name='instructions' value={instructions} />
-        <Form.Input fluid label='Recipe image url' name='image' value={image}/>
-        <Button type='submit'>Submit</Button>
-      </Form>
-    </Segment>
-    );
+    render() {
+      return (
+        <p>hello</p>
+      );
+    }
   }
 }
