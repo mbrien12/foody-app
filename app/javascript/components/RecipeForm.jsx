e13e6f25c965ef2d61fa681a15b8dbb7fc48b049
Michelle Brien <michelle.brien12@gmail.com>
2018-03-24 12:39:06 +0000

    added basic form fields

--- app/javascript/components/RecipeForm.jsx
+++ app/javascript/components/RecipeForm.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Form, Header,Segment } from "semantic-ui-react";
 
 export default class RecipeForm extends React.Component {
   render() {
+    const options = [
+      {key: 'b', text: 'Breakfast', value: 'breakfast'},
+      {key: 'l', text: 'Lunch', value: 'lunch'},
+      {key: 'd', text: 'Dinner', value: 'dinner'},
+      {key: 'de', text: 'Dessert', value: 'dessert'}
+    ]
+
+    //TODO: Fix label fonts
+    // const labelFont = {
+    //   fontWeight: 'lighter',
+    //   fontSize: '18px'
+    // }
+    
     return (
       <Segment inverted>
+      <Header as='h2'>Add recipe</Header>
       <Form inverted>
         <Form.Group widths='equal'>
-          <Form.Input fluid label='First name' placeholder='First name' />
-          <Form.Input fluid label='Last name' placeholder='Last name' />
+          <Form.Input fluid label='Recipe name' placeholder='Lasagna' />
+          <Form.Input fluid label='How does it take?' placeholder='40 mins' />
+          <Form.Input type='number' label='How many people is it for?' placeholder='4' />
         </Form.Group>
-        <Form.Checkbox label='I agree to the Terms and Conditions' />
+        <Form.Group widths='equal'>
+        <Form.Select fluid label='What type of meal is it?' options={options} placeholder='Lunch' />
+        <Form.Input fluid label='Where is it from?' placeholder='Guardian' />
+        </Form.Group>
+        <Form.TextArea label='Ingredients' />
+        <Form.TextArea label='Instructions' />
+        <Form.Input fluid label='Recipe image url'/>
         <Button type='submit'>Submit</Button>
       </Form>
     </Segment>
